bef18ea291aea8c6053e18dcfc2be00e40c568ce
Sannoob <cotwo0139@gmail.com>
2020-08-03 14:03:42 +0900

    Added Simple Cache

---
+++ src/structures/Cache.js
@@ -0,0 +1,22 @@
+class Cache extends Map {
+  /**
+     * @typedef {{ ttl: Number }} CacheOptions
+     * @param {CacheOptions} options - Cache Options
+     */
+  constructor (options) {
+    super()
+    this.ttl = options.ttl
+  }
+
+  get (key) {
+    super.get(key)
+  }
+
+  set (key, value) {
+    super.set(key, value)
+    setTimeout(() => {
+      this.delete(key)
+    }, this.ttl)
+  }
+}
+module.exports = Cache
