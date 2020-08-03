9864dfc1560ae9396d41e37df93fad96ddb2cc68
Sannoob <cotwo0139@gmail.com>
2020-08-03 15:40:25 +0900

    Added done() TEST Script

--- tests/SoundCloud.test.js
+++ tests/SoundCloud.test.js
@@ -1,12 +1,22 @@
 describe('SoundCloudExtractor', () => {
   const SoundCloudExtractor = require('../src/extractor/SoundCloudExtractor')
   const SoundCloud = new SoundCloudExtractor()
-  it('Parse Soundcloud scripts from soundcloud html', (done) => {
-    SoundCloud.parseScriptURLs().then(() => {
-      done()
-    }).catch(done)
-  })
-  it('Parse client_id from soudcloud scripts', (done) => {
-    SoundCloud.parseClientID().then()
+  describe('Parser', () => {
+    it('Parse Soundcloud scripts from soundcloud html', (done) => {
+      SoundCloud.parseScriptURLs().then(() => {
+        done()
+      }).catch(done)
+    })
+    it('Parse client_id from soudcloud scripts', (done) => {
+      SoundCloud.parseClientID().then(() => {
+        done()
+      }).catch(done)
+    })
+    describe('Others', () => {
+      it('Cache', (done) => {
+        if (SoundCloud.cache.size < 0) done(new Error('Cache TEST Failed'))
+        else done()
+      })
+    })
   })
 })
